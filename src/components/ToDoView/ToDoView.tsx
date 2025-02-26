0bb28f2ed58a4ae9d970003d4942d323a9792818
Gabriel <gabrielmaranhao.cpm@gmail.com>
2025-02-26 15:59:15 -0300

    feat: To Do List

--- src/components/ToDoView/ToDoView.tsx
+++ src/components/ToDoView/ToDoView.tsx
@@ -5,16 +5,22 @@ import CompletedCheckbox from "../../assets/checkedCheckbox.svg"
 import EditIcon from "../../assets/editIcon.svg"
 import DeleteIcon from "../../assets/deleteIcon.svg"
 import { ToDoItemData } from "../../types/toDoItem"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import EditModal from "../Modals/EditModal/EditModal"
 import DeleteModal from "../Modals/DeleteModal/DeleteModal"
+import { useLocation } from 'react-router-dom'
 
 function ToDo() {
   const [toDos, setToDos] = useState<ToDoItemData[]>([])
+  const [completedTasks, setCompletedTasks] = useState<ToDoItemData[]>([])
+  const [uncompletedTasks, setUncompletedTasks] = useState<ToDoItemData[]>([])
   const [newToDo, setNewToDo] = useState<string>("")
   const [showEditModal, setShowEditModal] = useState<boolean>(false)
   const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false)
   const [currentTaskId, setCurrentTaskId] = useState<string>("")
+  const [isLoaded, setIsLoaded] = useState<boolean>(false)
+
+  const location = useLocation()
 
   const addToDoItem = () => {
     if(newToDo !== "") {
@@ -77,68 +83,145 @@ function ToDo() {
     setCurrentTaskId("")
   }
 
-  // Função para pegar o título da tarefa atual
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      addToDoItem()
+    }
+  }
+
   const getCurrentTaskTitle = (): string => {
     const task = toDos.find(toDo => toDo.id === currentTaskId)
     return task ? task.title : ""
   }
 
-  return (
-    <main className='d-flex fd-column jc-center al-center'>
-      <div className='margin-b margin-t'>
-        <h1>Otimize seu tempo e se organize com o nosso Planejador Diário.</h1>
-      </div>  
-      <div className="todo-list">
-        <div className='d-flex list-title'>
-          <h3 className='tarefas-column'>Tarefa</h3>
-          <h3 className='status-column d-flex jc-center'>Status</h3>
-          <h3 className='opcoes-column d-flex jc-flex-end'>Opções</h3>
-        </div>
-        <div className="list-items">
-          {
-            toDos.map((toDo) => (
-              <div key={toDo.id} className="d-flex al-center todo-item">
-                <p className="item-title">{toDo.title}</p>
-                <div className="item-checkbox d-flex jc-center">
-                <img onClick={() => completeTask(toDo.id)} className="c-pointer" src={!toDo.completed ? Checkbox : CompletedCheckbox} alt="checkbox"/>
-                </div>
-                <div className="d-flex jc-flex-end option-itens">
-                  <img onClick={() => openEditModal(toDo.id)} className="c-pointer" src={EditIcon} alt="edit"/>
-                  <img onClick={() => openDeleteModal(toDo.id)} className="c-pointer" src={DeleteIcon} alt="delete"/>
-                </div>
-              </div>
-            ))
-          }
-        </div>
-      </div>
-      <div className="d-flex jc-space-btw al-center add-item">
-        <input 
-          value={newToDo} 
-          onChange={(e) => setNewToDo(e.target.value)} 
-          className="add-item-input" 
-          type="text"
-          placeholder="Nova tarefa..."
-        >
-        </input>
-        <p className="c-pointer" onClick={addToDoItem}>+</p>
-      </div>
+  useEffect(() => {
+    if(isLoaded) {
+      localStorage.setItem('tasks', JSON.stringify(toDos))
+    }
+  }, [toDos, isLoaded])
 
-      {showEditModal && (
-        <EditModal
-          taskTitle={getCurrentTaskTitle()}
-          onConfirm={editTask}
-          onCancel={closeModal}
-        />
-      )}
+  useEffect(() => {
+    const memoryTasks = localStorage.getItem('tasks')
+    if(memoryTasks) {
+      setToDos(JSON.parse(memoryTasks))
+    }
+    setIsLoaded(true)
+  }, [])
 
-      {showDeleteModal && (
-        <DeleteModal 
-          onConfirm={deleteTask}
-          onCancel={closeModal}
-        />
-      )}
-    </main>
-  )
+  useEffect(() => {
+    const filterCompletedTasks = toDos.filter((toDo) => toDo.completed === true)
+    setCompletedTasks(filterCompletedTasks)
+  }, [toDos])
+
+  useEffect(() => {
+    const filterUncompletedTasks = toDos.filter((toDo) => toDo.completed === false)
+    setUncompletedTasks(filterUncompletedTasks)
+  }, [toDos])
+
+  if (location.pathname === "/tarefas") {
+    return (
+      <main className='d-flex fd-column jc-center al-center'>
+        <div className='margin-b margin-t'>
+          <h1>Otimize seu tempo e se organize com o nosso Planejador Diário.</h1>
+        </div>  
+        <div className="todo-list">
+          <div className='d-flex list-title'>
+            <h3 className='tarefas-column'>Tarefa</h3>
+            <h3 className='status-column d-flex jc-center'>Status</h3>
+            <h3 className='opcoes-column d-flex jc-flex-end'>Opções</h3>
+          </div>
+          <div className="list-items">
+            {
+              toDos.map((toDo) => (
+                <div key={toDo.id} className="d-flex al-center todo-item">
+                  <p style={{ textDecoration: toDo.completed ? 'line-through' : 'none'}} className="item-title">{toDo.title}</p>
+                  <div className="item-checkbox d-flex jc-center">
+                  <img onClick={() => completeTask(toDo.id)} className="c-pointer" src={!toDo.completed ? Checkbox : CompletedCheckbox} alt="checkbox"/>
+                  </div>
+                  <div className="d-flex jc-flex-end option-itens">
+                    <img onClick={() => openEditModal(toDo.id)} className="c-pointer" src={EditIcon} alt="edit"/>
+                    <img onClick={() => openDeleteModal(toDo.id)} className="c-pointer" src={DeleteIcon} alt="delete"/>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+        <div className="d-flex jc-space-btw al-center add-item">
+          <input 
+            value={newToDo} 
+            onChange={(e) => setNewToDo(e.target.value)} 
+            onKeyPress={handleKeyPress}
+            className="add-item-input" 
+            type="text"
+            placeholder="Nova tarefa..."
+          >
+          </input>
+          <p className="c-pointer" onClick={addToDoItem}>+</p>
+        </div>
+
+        {showEditModal && (
+          <EditModal
+            taskTitle={getCurrentTaskTitle()}
+            onConfirm={editTask}
+            onCancel={closeModal}
+          />
+        )}
+
+        {showDeleteModal && (
+          <DeleteModal 
+            onConfirm={deleteTask}
+            onCancel={closeModal}
+          />
+        )}
+      </main>
+    )
+  } else if (location.pathname === "/organizacao") {
+    return (
+      <main className='d-flex fd-column jc-center al-center'>
+        <div className='margin-b margin-t'>
+          <h1>Otimize seu tempo e se organize com o nosso Planejador Diário.</h1>
+        </div>
+        <section className="d-flex fd-row jc-center tasks-resume">
+          <div>
+            <h3>Tarefas a fazer</h3>
+            <ul>
+              {
+                uncompletedTasks.map((toDo) => (
+                  <li>
+                    <div key={toDo.id} className="d-flex al-center todo-item">
+                      <p style={{ textDecoration: toDo.completed ? 'line-through' : 'none'}} className="item-title">{toDo.title}</p>
+                      <div className="item-checkbox d-flex jc-center">
+                        <img onClick={() => completeTask(toDo.id)} className="c-pointer" src={!toDo.completed ? Checkbox : CompletedCheckbox} alt="checkbox"/>
+                      </div>
+                    </div>
+                  </li>
+                ))
+              }
+            </ul>
+          </div> 
+          <div>
+            <h3>Tarefas completas</h3>
+            <ul>
+              {
+                completedTasks.map((toDo) => (
+                  <li>
+                    <div key={toDo.id} className="d-flex al-center todo-item">
+                      <p style={{ textDecoration: toDo.completed ? 'line-through' : 'none'}} className="item-title">{toDo.title}</p>
+                      <div className="item-checkbox d-flex jc-center">
+                        <img onClick={() => completeTask(toDo.id)} className="c-pointer" src={!toDo.completed ? Checkbox : CompletedCheckbox} alt="checkbox"/>
+                      </div>
+                    </div>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        </section>
+      </main>
+    )
+  } 
+
 }
 
 export default ToDo
